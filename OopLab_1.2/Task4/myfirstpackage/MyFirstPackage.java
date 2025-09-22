c67e8b27ae3cd48c1b5b43c57fe56eb0a0dcad69
maks23415 <maks4568jk@mail.ru>
2025-09-22 18:26:25 +0400

    Add files via upload

---
+++ OopLab_1.2/Task4/myfirstpackage/MyFirstPackage.java
@@ -0,0 +1,36 @@
+package myfirstpackage;
+
+public class MyFirstPackage{
+	private int number1;
+	private int number2;
+	
+	public MyFirstPackage(int numbe1, int numbe2)
+	{
+	this.number1=numbe1;
+	this.number2=numbe2;
+	}
+	
+	public int getNumber1()
+	{
+	return number1;
+	}
+	
+	public int getNumber2()
+	{
+	return number2;
+	}
+	
+	public void setNumber1(int num1){
+	this.number1=num1;
+	}
+	
+	public void setNumber2(int num2){
+	this.number2=num2;
+	}
+	
+	public int multiply(){
+	return number1*number2;
+	}
+	
+}
+	
